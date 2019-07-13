41b9a2a812d27f52277ac0064d228a05095d4fcc
lunanyan <chkong1998@gmail.com>
2019-07-13 16:50:26 +0900

    some fix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import discord, asyncio, configparser, datetime, re, os
+import discord, asyncio, configparser, datetime, re, os, sys
 
 bot_ver = "1.0.1b"
 
@@ -21,15 +21,10 @@ async def bgjob_change_playing():
         await client.change_presence(game=discord.Game(name=str(members_sum) + ' users available'))
         await asyncio.sleep(10)
         await client.change_presence(game=discord.Game(name='v' + bot_ver))
-        await asyncio.sleep(10)
-        await client.change_presence(game=discord.Game(name=str(os.popen('uptime -p').read())))
-        await asyncio.sleep(10)
-        await client.change_presence(game=discord.Game(name=str(os.popen('cat /proc/loadavg').read())))
-        await asyncio.sleep(10)
-        await client.change_presence(game=discord.Game(name=str(os.popen('/opt/vc/bin/vcgencmd measure_temp').read())))
 
 @client.event
 async def on_ready():
+    print('bot is ready to overwatch.')
     print('name    : ' + str(client.user.name))
     print('id      : ' + str(client.user.id))
     print('version : ' + bot_ver)
@@ -46,16 +41,8 @@ async def on_message(message):
         hs_match = hatespeech.match(message.content)
         if hs_match:
             await client.send_message(discord.Object(id=db.get('config', 'alert_channel_id')), "possible hate speech found at " + message.channel.name + "\n" + message.author.display_name + " : " + message.content + "\nat : " + str(datetime.datetime.now()))
-        if str(message.channel.id) == db.get('config', 'shell_channel_id') and message.author.id == db.get('config', 'owner_id'):
-            if message.content.startswith('cputemp'):
-                await client.send_message(message.channel, str(os.popen('/opt/vc/bin/vcgencmd measure_temp').read()))
-            else:
-                shl_str = message.content
-                shl_str = shl_str.replace('hauzen execute ','')
-                try:
-                    await client.send_message(message.channel, str(os.popen(shl_str).read()))
-                except:
-                    await client.send_message(message.channel, ':facepalm:')
+            if db.get('config', 'delete_hatespeech') == '1':
+                await client.delete_message(message)
     else:
         await client.leave_server(message.server)
     with open(db_path, 'w') as configfile:
